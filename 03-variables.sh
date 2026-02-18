904e2d4c131e6fb7912de2273ed3c53e44aacbbd
Indyalamounika <vedamouni01@gmail.com>
2026-02-18 16:12:26 +0530

    shell-basics

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#no space before or after equal
+PERSON1=mounika
+PERSON2=krupa
+
+echo "PERSON1:: hii PERSON2, how are?"
+echo "PERSON2:: hello PERSON1, i am good ,what about you?"
+echo "PERSON1:: what are you learning recently?"
+echo "PERSON2:: iam  leaning shell scrpiting PERSON1"
