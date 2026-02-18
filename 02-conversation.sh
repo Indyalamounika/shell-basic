48359c1b2849509746f959da57a186da4d8e7994
Indyalamounika <vedamouni01@gmail.com>
2026-02-18 16:04:56 +0530

    shell-basics

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 echo "mounika:: hii krupa, how are?"
-echo "kupra:: hello mounika, i am good ,what about you?"
+echo "krupa:: hello mounika, i am good ,what about you?"
+echo "mounika:: what are you learning recently?"
+echo "krupa:: iam  leaning shell scrpiting mounika"
