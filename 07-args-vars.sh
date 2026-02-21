1b7c1b5c9654101d4f833d9951a71e3d6d884af5
Indyalamounika <vedamouni01@gmail.com>
2026-02-21 16:44:30 +0530

    shell

---
+++ 07-args-vars.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Arguments
+PERSON1=$1
+PERSON2=$2
+PERSON3=$3
+
+echo "$PERSON1::welocom to indaia $PERSON2"
+echo "$PERSON2::thankyou $PERSON1, finally"
+echo "$PERSON3::so, do we have"
+echo "$PERSON2::yes $PERSON1"
