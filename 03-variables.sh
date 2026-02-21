668c80bf506c3229cbf8fa7248e190c47aba7400
Indyalamounika <vedamouni01@gmail.com>
2026-02-21 15:58:07 +0530

    shell

--- 03-variables.sh
+++ 03-variables.sh
@@ -4,7 +4,7 @@
 PERSON1=mounika
 PERSON2=krupa
 
-echo "PERSON1:: hii PERSON2, how are?"
-echo "PERSON2:: hello PERSON1, i am good ,what about you?"
-echo "PERSON1:: what are you learning recently?"
-echo "PERSON2:: iam  leaning shell scrpiting PERSON1"
+echo "$PERSON1:: hii $PERSON2, how are?"
+echo "$PERSON2:: hello $PERSON1, i am good ,what about you?"
+echo "$PERSON1:: what are you learning recently?"
+echo "$PERSON2:: iam  leaning shell scrpiting $PERSON1"
