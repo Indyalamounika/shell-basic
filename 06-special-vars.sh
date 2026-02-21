0bc7a16ed32c75a171a61acf74246c28869b82f5
Indyalamounika <vedamouni01@gmail.com>
2026-02-21 16:58:31 +0530

    shell

--- 06-special-vars.sh
+++ 06-special-vars.sh
@@ -3,7 +3,7 @@
 echo "Script name: $0"
 echo "Default shell: $SHELL"
 echo "All variables passed to the script: $@"
-echo "All variables passed to the script: $*"
+echo "All variables passed to the script: $#"
 echo "Current directory: $PWD"
 echo "Who is running this: $USER"
 echo "Home directory of user: $HOME"
