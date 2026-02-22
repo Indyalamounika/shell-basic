34c9234a73674975e2dc5178a6e960f1c470b2f9
Indyalamounika <vedamouni01@gmail.com>
2026-02-22 16:07:00 +0530

    shell

--- 09-datatypes.sh
+++ 09-datatypes.sh
@@ -7,3 +7,9 @@ NAME=Mounika
 SUM=$(($NUMBER1 + $NUMBER2 + $NAME))
 
 echo "SUM is: ${SUM}"
+
+FRUITES=("Apple" "Banana" "Grape" "Watermilloan")
+
+echo "All fruites: ${FRUITES[@]}"
+echo "First Fruite: ${FRUITES[0]}"
+
