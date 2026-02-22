c7dc7f6dd9d7b7f010c8a83155811fe72c420fe5
Indyalamounika <vedamouni01@gmail.com>
2026-02-22 15:55:43 +0530

    shell

--- 08-variable.sh
+++ 08-variable.sh
@@ -2,15 +2,12 @@
 
 # Storing current date into a variable
 # DATE=$(date)
-
 # # Printing the value
 # echo "Today's date is: $DATE"
 
+
 START_TIME=$(date +%S)
-
 sleep 5
-
 END_TIME=$(date +%S)
 TOTAL_TIME=$(($END_TIME-$START_TIME))
-
 echo "script executed in: $TOTAL_TIME Seconds"
---
+++ 09-datatypes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+NUMBER1 =100
+NUMBER2 =500
+
+SUM=$(($NUMBER1+$NUMBER2))
+echo "SUM is= ${sum}
