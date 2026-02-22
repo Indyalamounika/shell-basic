7ef6cce362f90f0e7ca9c7b09010773910822dea
Indyalamounika <vedamouni01@gmail.com>
2026-02-22 15:45:59 +0530

    shell

--- 08-variable.sh
+++ 08-variable.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 # Storing current date into a variable
-DATE=$(date)
+# DATE=$(date)
 
-# Printing the value
-echo "Today's date is: $DATE"
+# # Printing the value
+# echo "Today's date is: $DATE"
+
+START_TIME=$(date +%S)
+
+sleep 10
+
+END_TIME=$(date +%S)
+TOTAL_TIME=$($END_TIME-$START_TIME)
+
+echo "script executed in: $TOTAL_TIME Seconds"
