a89dd0b40f874ca971986b1b69a1b5c9099aa403
Indyalamounika <vedamouni01@gmail.com>
2026-02-22 15:27:27 +0530

    shell

--- 06-special-vars.sh
+++ 06-special-vars.sh
@@ -4,9 +4,11 @@ echo "Script name: $0"
 echo "Default shell: $SHELL"
 echo "All variables passed to the script: $@"
 echo "All variables passed to the script: $#"
+echo "All variables passed to the script: $*"
 echo "Current directory: $PWD"
 echo "Who is running this: $USER"
 echo "Home directory of user: $HOME"
 echo "PID of this script: $$"
 sleep 10 &
 echo "PID of the last command in background is: $!"
+
