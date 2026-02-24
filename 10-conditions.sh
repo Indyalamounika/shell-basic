95df1e09e310fa4d05495894d43b78ac3d40027c
Indyalamounika <vedamouni01@gmail.com>
2026-02-24 15:42:53 +0530

    shell

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -4,6 +4,8 @@ NUMBER=$1
 
 if [ $NUMBER -lt 5 ]; then
     echo "Given number $NUMBER is less than 5"
+elif [ $NUMBER -eq 5 ]
+    echo "Given number $NUMBER is equal to 5"
 else
     echo "Given number $NUMBER is greater than to 5"
 fi
