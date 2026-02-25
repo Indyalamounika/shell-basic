bebf0af38ba721c409c1ca6be82bd60562132d8c
Indyalamounika <vedamouni01@gmail.com>
2026-02-25 16:10:25 +0530

    shell

---
+++ 17-logs.sh
@@ -0,0 +1,53 @@
+#!bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-scrpit"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # var/log/shell-scrpit/17-logs.log
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
+
+VALIDATE(){ #functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo "ERROR:: Installing $2 ... $R FAILURE $N" | $LOG_FILE
+        exit 1
+    else
+        echo "Installing $2 ... $G SUCCESS $N" | $LOG_FILE
+fi
+}
+
+dnf list installed mysql &>>$LOG_FILE
+# install if it is not found
+if  [ $? -ne 0 ]; then
+    dnf install mysql -y &>>$LOG_FILE
+    VALIDATE $? "MySQL"
+else 
+    echo -e "MySQL already exit ...$Y SKIPPING $N" &>>$LOG_FILE
+fi
+
+dnf list installed python3 &>>$LOG_FILE
+if  [ $? -ne 0 ]; then
+    dnf install python3 -y &>>$LOG_FILE
+    VALIDATE $? "Python3"
+else 
+    echo -e "Python3 already exit ...$Y SKIPPING $N" &>>$LOG_FILE
+fi
+
+dnf list installed nginx &>>$LOG_FILE
+if  [ $? -ne 0 ]; then
+    dnf install nginx -y &>>$LOG_FILE
+    VALIDATE $? "Nginx"
+else 
+    echo -e "Nginx already exit ...$Y SKIPPING $N" &>>$LOG_FILE
+fi
